d91bef704b2d8ee122f464e4bfaa8eeee272f877
Victor <victorchike247@gmail.com>
2023-01-11 09:28:39 +0000

    made logo a home link

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -4,9 +4,9 @@
             <button class="lg:hidden">
                 <hamburger-icon></hamburger-icon>
             </button>
-            <figure class="ml-[7.6rem] mr-[6.9rem] md:ml-[4.2rem] md:mr-auto lg:ml-0 lg:mr-0">
+            <router-link :to="{name: 'home'}" class="ml-[7.6rem] mr-[6.9rem] md:ml-[4.2rem] md:mr-auto lg:ml-0 lg:mr-0">
                 <logo-icon></logo-icon>
-            </figure>
+            </router-link>
             <div class="hidden lg:flex gap-3.4 ml-[19.7rem] mr-auto">
                 <router-link :to="{name: 'home'}" class="text-white hover:text-brown uppercase text-xs font-bold leading-sm tracking-px">Home</router-link>
                 <router-link :to="{name: 'category', params: {category: 'headphones'}}" class="text-white hover:text-brown uppercase text-xs font-bold leading-sm tracking-px">Headphones</router-link>
