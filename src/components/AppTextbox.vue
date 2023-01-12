4d064501971f3553078b89543d64ab59daecc283
Victor <victorchike247@gmail.com>
2023-01-12 14:08:06 +0000

    added form validation

--- src/components/AppTextbox.vue
+++ src/components/AppTextbox.vue
@@ -1,38 +1,33 @@
 <template>
-    <div class="flex flex-col gap-4">
-        <div  v-if="labelText" class="flex justify-between">
-            <label :for="id || labelText" :class="{'text-brown-deep': hasError}" class="font-bold text-black text-[1.2rem] leading-[1.6rem] -tracking-[.21px]">{{labelText}}</label>
-            <span v-if="hasError" class="text-brown-deep text-[1.2rem] leading-[1.6rem] -tracking-[.21px] font-semibold">{{ errorMessage || `can't be empty`}}</span>
+    <Field :name="id" v-slot="{ field, meta, errorMessage }">
+        <div class="flex flex-col gap-4">
+            <div class="flex justify-between">
+                <label :for="id" :class="{'text-brown-deep': meta.dirty && !meta.valid}" class="font-bold text-black text-[1.2rem] leading-[1.6rem] -tracking-[.21px]">{{labelText}}</label>
+                <span v-show="meta.dirty && !meta.valid" class="text-brown-deep text-[1.2rem] leading-[1.6rem] -tracking-[.21px] font-semibold">{{ errorMessage }}</span>
+            </div>
+            <input
+                v-bind="field"
+                :id="id"
+                :class="{'border-2 border-brown-deep focus:border-brown-deep': meta.dirty && !meta.valid , 'border border-grey-deep focus:border-brown': !(meta.dirty && !meta.valid)}"
+                class="bg-white caret-brown hover:border-brown hover:cursor-pointer focus:ring-0
+                text-black text-xsm leading-xs -tracking-[.25px] font-bold
+                rounded-0.8 px-2.4 h-5.6 focus-visible:outline-none placeholder:text-black/40 " 
+                :type="type || 'text'"
+                :placeholder="placeholder"
+            />
         </div>
-        
-        <input 
-            :id="id || labelText"
-            :class="{'border-2 border-brown-deep focus:border-brown-deep': hasError , 'border border-grey-deep focus:border-brown': !hasError}"
-            class="bg-white caret-brown hover:border-brown hover:cursor-pointer focus:ring-0
-            text-black text-xsm leading-xs -tracking-[.25px] font-bold
-            rounded-0.8 px-2.4 h-5.6 focus-visible:outline-none placeholder:text-black/40 "
-            :value="modelValue"
-            @input="$emit('update:modelValue', $event.target ? ($event.target as HTMLInputElement).value : '')" 
-            type="text"
-            :placeholder="placeholder"
-        >
-    </div>
+    </Field>
 </template>
 
 <script setup lang="ts">
-interface Props {
-    modelValue?: string | number;
-    labelText?: string;
-    id?: string;
+import { Field } from 'vee-validate';
+defineProps<{
+    labelText: string;
+    id: string;
+    type?: 'text' | 'email';
     placeholder?: string
-    hasError?: boolean;
-    errorMessage?: string; 
-}
-interface Emits {
-    (e: 'update:modelValue', val: string | number): any
-}
-defineProps<Props>()
-defineEmits<Emits>()
+}>()
+
 </script>
 
 <style scoped>
--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="mt-6.4 mb-9.5 md:mt-9.5 md:mb-[11.4rem] lg:my-56 flex flex-col lg:flex-row gap-3.2 lg:gap-12 px-2.4 md:px-0 md:w-cm lg:w-ct mx-0 md:mx-auto">
+    <Form @submit="submit" :validation-schema="schema" class="mt-6.4 mb-9.5 md:mt-9.5 md:mb-[11.4rem] lg:my-56 flex flex-col lg:flex-row gap-3.2 lg:gap-12 px-2.4 md:px-0 md:w-cm lg:w-ct mx-0 md:mx-auto">
         <div class="lg:flex-1 bg-white p-2.4 md:px-11 md:py-12 lg:px-4.8 lg:pb-4.8 lg:pt-[5.4rem] rounded-0.8">
             <h2 class="font-bold text-black text-lgm md:text-xl leading-xl md:leading-lg tracking-xp md:tracking-xpm uppercase mb-3.2 md:mb-16">checkout</h2>
             <section class="mb-3.2 md:mb-[5.3rem] flex flex-col gap-1.6">
                 <h3 class="text-brown font-bold text-xs leading-sm tracking-xp uppercase">billing details</h3>
                 <div class="grid md:grid-cols-2 gap-y-2.4 md:gap-x-1.6">
-                    <Textbox label-text="Name" placeholder="Alexei Ward"/>
-                    <Textbox label-text="Email" placeholder="alexei@mail.com"/>
-                    <Textbox label-text="Phone Number" placeholder="+1 202-555-0136"/>
+                    <Textbox label-text="Name" id="name" placeholder="Alexei Ward"/>
+                    <Textbox label-text="Email" id="email" type="email" placeholder="alexei@mail.com"/>
+                    <Textbox label-text="Phone Number" id="phone" placeholder="+1 202-555-0136"/>
                 </div>
             </section>
             <section class="mb-3.2 md:mb-24 flex flex-col gap-1.6">
                 <h3 class="text-brown font-bold text-xs leading-sm tracking-xp uppercase">shipping info</h3>
                 <div class="grid md:grid-cols-2 gap-y-2.4 md:gap-x-1.6">
-                    <Textbox label-text="Address" placeholder="1137 Williams Avenue" class="lg:col-span-2"/>
-                    <Textbox label-text="Zip Code" placeholder="10001"/>
-                    <Textbox label-text="City" placeholder="New York"/>
-                    <Textbox label-text="Country" placeholder="United States"/>
+                    <Textbox label-text="Address" id="address" placeholder="1137 Williams Avenue" class="lg:col-span-2"/>
+                    <Textbox label-text="Zip Code" id="zipCode" placeholder="10001"/>
+                    <Textbox label-text="City" id="city" placeholder="New York"/>
+                    <Textbox label-text="Country" id="country" placeholder="United States"/>
                 </div>
             </section>
             <section>
@@ -27,8 +27,8 @@
                     <RadioButton label-text="Cash on Delivery" v-model="selectedPayment" name="payment method" class="md:col-start-2"/>
                 </div>
                 <div v-show="selectedPayment === eMoney" class="grid md:grid-cols-2 gap-y-2.4 md:gap-x-1.6 mt-3.2 md:mt-2.4">
-                    <Textbox label-text="e-Money Number" placeholder="238521993"/>
-                    <Textbox label-text="e-Money PIN" placeholder="6891"/>  
+                    <Textbox label-text="e-Money Number" id="eMoneyNumber" placeholder="238521993"/>
+                    <Textbox label-text="e-Money PIN" id="eMoneyPin" placeholder="6891"/>  
                 </div>
                 <div v-show="!(selectedPayment === eMoney)" class="flex flex-col md:flex-row gap-2.4 md:gap-12 2 mt-3.2 md:mt-12">
                     <cash-on-delivery-icon class="self-center"></cash-on-delivery-icon>
@@ -60,9 +60,9 @@
                 </div>
             </div>
             
-            <button @click="showModal" class="btn btn-primary btn-full">continue & pay</button>
+            <button class="btn btn-primary btn-full">continue & pay</button>
         </div>
-    </div>
+    </Form>
     <Teleport to="body">
       <OrderConfirmModal :show="orderCompleted" @close="returnToHomepage" />
     </Teleport>
@@ -75,7 +75,20 @@ import CashOnDeliveryIcon from '@/components/icons/IconCashOnDelivery.vue';
 import CartItem from '@/components/CartItem.vue';
 import OrderConfirmModal from '@/components/OrderConfirmModal.vue';
 import { useRouter } from 'vue-router'
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import { Form } from 'vee-validate';
+import * as yup from 'yup';
+
+const emptyLabel = ' '
+const baseSchema = yup.object({
+    name: yup.string().required().label(emptyLabel),
+    email: yup.string().required().email().label(emptyLabel),
+    phone: yup.string().required().label(emptyLabel),
+    address: yup.string().required().label(emptyLabel),
+    city: yup.string().required().label(emptyLabel),
+    zipCode: yup.string().required().label(emptyLabel),
+    country: yup.string().required().label(emptyLabel),
+});
 
 const eMoney = 'e-Money'
 const arr = [1, 2, 3]
@@ -83,10 +96,33 @@ const selectedPayment = ref(eMoney)
 const orderCompleted = ref(false)
 const router = useRouter()
 
+const schema = computed(() => {
+    if (selectedPayment.value === eMoney) {
+        return yup.object({
+            name: yup.string().required().label(emptyLabel),
+            email: yup.string().required().email().label(emptyLabel),
+            phone: yup.string().required().label(emptyLabel),
+            address: yup.string().required().label(emptyLabel),
+            city: yup.string().required().label(emptyLabel),
+            zipCode: yup.string().required().label(emptyLabel),
+            country: yup.string().required().label(emptyLabel),
+            eMoneyNumber: yup.string().required().label(emptyLabel),
+            eMoneyPin: yup.string().required().label(emptyLabel),
+        });
+    }
+    
+    return baseSchema
+})
+
 const showModal = () => orderCompleted.value = true
 
 const returnToHomepage = () => {
     orderCompleted.value = false
     router.push({name: 'home'})
+}
+
+const submit = (values: any) => {
+    console.log(values)
+    showModal()
 }
 </script>
